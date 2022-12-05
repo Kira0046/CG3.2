06fbd5dbc988620133ea4f811b2dd6b3a837937b
Kira003 <k021g1248@g.neec.ac.jp>
2022-12-05 11:26:47 +0900

    プロジェクト ファイルを追加します。

---
+++ Resources/shaders/OBJVertexShader.hlsl
@@ -0,0 +1,18 @@
+#include "OBJShaderHeader.hlsli"
+
+VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
+{
+	float3 lightdir = float3(1, -1, 1);
+	lightdir = normalize(lightdir);
+
+	float3 lightcolor = float3(1, 1, 1);
+
+	VSOutput output; // ピクセルシェーダーに渡す値
+	output.svpos = mul(mat, pos);
+
+	output.color.rgb = dot(-lightdir, normal) * m_diffuse * lightcolor;
+	output.color.a = m_alpha;
+	output.uv = uv;
+
+	return output;
+}
